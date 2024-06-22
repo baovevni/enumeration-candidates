fdc29a7e6e0800021e888cff47a2f310c436fc41
Evgheni Barducov <52833790+baovevni@users.noreply.github.com>
2024-06-21 20:34:22 -0400

    Implemented filters

--- filters.rb
+++ filters.rb
@@ -2,15 +2,42 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
+  @candidates.find { |candidate| candidate[:id] == id }
+end
+
+def experienced?(candidate)
+  candidate[:years_of_experience] >= 2
+end
+
+def high_github_points?(candidate)
+  candidate[:github_points] >= 100
+end
+
+def knows_required_languages?(candidate)
+  required_languages = ['Ruby', 'Python']
+  (candidate[:languages] & required_languages).any?
+end
+
+def recently_applied?(candidate)
+  candidate[:date_applied] >= 15.days.ago.to_date
+end
+
+def of_age?(candidate)
+  candidate[:age] > 17
+end
+
+def qualified_candidates(candidates)
+  candidates.select do |candidate|
+    experienced?(candidate) &&
+    high_github_points?(candidate) &&
+    knows_required_languages?(candidate) &&
+    recently_applied?(candidate) &&
+    of_age?(candidate)
   end
-  
-  def experienced?(candidate)
-    # Your code Here
-  end
-  
-  def qualified_candidates(candidates)
-    # Your code Here
-  end
-  
-  # More methods will go below
+end
+
+def ordered_by_qualifications(candidates)
+  candidates.sort_by { |candidate| [-candidate[:years_of_experience], -candidate[:github_points]] }
+end
+
+# More methods will go below
--- main.rb
+++ main.rb
@@ -1,9 +1,26 @@
-# This is the main entrypoint into the program
-# It requires the other files/gems that it needs
 require 'pp'
-require './candidates'
-require './filters'
+require 'active_support/all'
+require_relative 'candidates'
+require_relative 'filters'
 
-## Your test code can go here
-
+# List out the candidates
 pp @candidates
+
+# Test experienced? method
+candidate = @candidates.first
+puts "Is the first candidate experienced? #{experienced?(candidate)}"
+
+# Find a candidate by ID
+candidate_id = 1 # Use a real ID from your candidates
+candidate = find(candidate_id)
+if candidate
+  puts "Found candidate with ID #{candidate_id}:"
+  pp candidate
+else
+  puts "No candidate found with ID #{candidate_id}."
+end
+
+# List out the qualified candidates
+qualified_list = qualified_candidates(@candidates)
+puts "Qualified candidates:"
+pp qualified_list
